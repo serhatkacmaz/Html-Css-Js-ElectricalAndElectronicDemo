c3c56523cfbaa219436a76b3bba7c0cd340c15ff
Serhatkacmaz <56757412+Serhatkacmaz@users.noreply.github.com>
2021-11-19 16:38:24 +0300

    duyuru ekleme popup yapıldı.

--- javascript/duyurular.js
+++ javascript/duyurular.js
@@ -37,3 +37,7 @@ function duyuru9() {
 function duyuru10() {
     document.getElementById("popup-10").classList.toggle("active");
 }
+
+function duyuruEkle() {
+    document.getElementById("popup-duyuru-ekle").classList.toggle("active");
+}
